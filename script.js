8de66b9fb09677679016905f5f943bc36cbbbe87
Natanael Silva <natanaeljazzmusic@gmail.com>
2026-01-21 04:19:45 -0400

    Lógica de contraste para o preto e exibição do Hex no botão

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ function mudarCordeFundo() {
     // Pegamos a cor da lista na posição do contador
     document.body.style.backgroundColor = cores[contador];
 
+    // Mudando o texto do botão para o código da cor atual
+    botao.textContent = "cor:" + cores[contador];
+
+    // SE a cor atual for o preto absoluto...
+if (cores[contador] === '#000000') {
+    // Mude a cor da letra do parágrafo para branco
+    document.querySelector('p').style.color = 'white';
+} else {
+    // CASO CONTRÁRIO, mantenha o vermelho escuro (ou a cor original)
+    document.querySelector('p').style.color = 'darkred';
+}
     // Somamos +1 ao contador para a próxima vez ser outra cor
     contador = contador + 1;
 
